d2312744498655ce0c498306f2af0b38bbedc776
tangollama <tangollama@gmail.com>
2014-10-29 14:12:24 -0400

    https support for vimeo and youtube
    modified the protocol string so that embedding in an https environment
    is possible.

--- src/js/media/jquery.tosrus.youtube.js
+++ src/js/media/jquery.tosrus.youtube.js
@@ -30,7 +30,7 @@
 
 			if ( this.opts[ _MEDIA_ ].imageLink )
 			{
-				href = 'http://img.youtube.com/vi/' + href + '/0.jpg';
+				href = '//img.youtube.com/vi/' + href + '/0.jpg';
 				$('<a href="' + url + '" class="' + $[ _PLUGIN_ ]._c( 'play' ) + '" target="_blank" />')
 					.appendTo( $s );
 
@@ -48,7 +48,7 @@
 			}
 			else
 			{
-				$('<iframe src="http://www.youtube.com/embed/' + href + '?enablejsapi=1" frameborder="0" allowfullscreen />')
+				$('<iframe src="//www.youtube.com/embed/' + href + '?enablejsapi=1" frameborder="0" allowfullscreen />')
 					.appendTo( $s );
 
 				initVideo.call( this, $s );
